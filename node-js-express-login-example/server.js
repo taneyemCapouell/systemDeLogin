232440eaaa4a94dfd7d82d4cb8178e6d742a47c6
taneyemCapouell <109171805+taneyemCapouell@users.noreply.github.com>
2023-07-25 08:11:25 +0200

    git initial

--- node-js-express-login-example/server.js
+++ node-js-express-login-example/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:6731');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-credentials', 'true');
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'http://localhost:6731',
+  origin: 'http://localhost:4200',
   credentials: true
 }));
 
